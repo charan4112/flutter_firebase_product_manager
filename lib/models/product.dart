7b888d94d3be4ac3ad0d3703ea7d51d9bb3b09eb
charan4112 <charanpendyala4112@gmail.com>
2025-03-26 15:45:46 -0400

    Added Product model and ProductService for Firestore operations

---
+++ lib/models/product.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Product {
+  final String id;
+  final String name;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+
+  // Create a Product from Firestore document data
+  factory Product.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Product(
+      id: doc.id,
+      name: data['name'] as String,
+      price: (data['price'] as num).toDouble(),
+    );
+  }
+
+  // Convert Product to a map for Firestore storage
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'price': price,
+    };
+  }
+}
---
+++ lib/services/product_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/product.dart';
+
+class ProductService {
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('products');
+
+  // Get a stream of products
+  Stream<List<Product>> getProducts() {
+    return _products.snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Product.fromFirestore(doc)).toList());
+  }
+
+  // Add a new product
+  Future<void> addProduct(Product product) {
+    return _products.add(product.toMap());
+  }
+
+  // Update an existing product
+  Future<void> updateProduct(Product product) {
+    return _products.doc(product.id).update(product.toMap());
+  }
+
+  // Delete a product by id
+  Future<void> deleteProduct(String productId) {
+    return _products.doc(productId).delete();
+  }
+}
