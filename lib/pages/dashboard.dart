e5303d478766272475532a72b7bea51a83eb9af4
charan4112 <charanpendyala4112@gmail.com>
2025-03-29 14:26:05 -0400

    Added initial Dashboard UI with navigation placeholder

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white10,
+      appBar: AppBar(
+        title: Text('DashBoard'),
+        centerTitle: true,
+        backgroundColor: Colors.black26,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Welcome to your finance!',
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              'Here your data appears...',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop();
+              },
+              child: Text('Back'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
